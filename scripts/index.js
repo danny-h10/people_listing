16b2521f0b2df2d8d1ede152239b28e184267f49
Daniel Hernandez <dh893177@gmail.com>
2024-05-17 14:09:54 -0500

    finished people_listing

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,39 @@
 
 window.onload = () => {
 
-    
+    loadPeopleTable();
 
+}
+
+function loadPeopleTable(){
+
+    let tableBody = document.querySelector("#peopleTblBody")
+
+    people.forEach( (person) =>{
+
+        buildTableRow(tableBody, person);
+
+    })
+
+
+function buildTableRow(tableBody, data){
+    
+    let newRow = tableBody.insertRow();
+
+    let cell1 = newRow.insertCell();
+    cell1.innerHTML = data.id
+
+    let cell2 = newRow.insertCell();
+    cell2.innerHTML = data.firstName
+
+    let cell3 = newRow.insertCell();
+    cell3.innerHTML = data.lastName
+
+    let cell4 = newRow.insertCell();
+    cell4.innerHTML = data.email
+
+    let cell5 = newRow.insertCell();
+    cell5.innerHTML = data.ipAddress
+
+}
 }
